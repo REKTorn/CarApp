66179900773a0897caaca23af4dd777f2611fe1b
REKTorn <emanuel.carlhed@gmail.com>
2021-04-15 11:19:48 +0200

    Done Coding

--- CarApp/Form1.Designer.cs
+++ CarApp/Form1.Designer.cs
@@ -29,24 +29,24 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.lsvCars = new System.Windows.Forms.ListView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.btnAdd = new System.Windows.Forms.Button();
-            this.btnClearAll = new System.Windows.Forms.Button();
-            this.btnClearMarked = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxForSale = new System.Windows.Forms.CheckBox();
+            this.tbxYear = new System.Windows.Forms.TextBox();
+            this.tbxModel = new System.Windows.Forms.TextBox();
+            this.tbxMake = new System.Windows.Forms.TextBox();
+            this.tbxRegNr = new System.Windows.Forms.TextBox();
+            this.lsvCars = new System.Windows.Forms.ListView();
             this.regnr = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.make = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.model = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.year = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.forsale = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.btnRemoveMarked = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,59 +57,110 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.checkBox1);
-            this.groupBox1.Controls.Add(this.textBox4);
-            this.groupBox1.Controls.Add(this.textBox3);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.cbxForSale);
+            this.groupBox1.Controls.Add(this.tbxYear);
+            this.groupBox1.Controls.Add(this.tbxModel);
+            this.groupBox1.Controls.Add(this.tbxMake);
+            this.groupBox1.Controls.Add(this.tbxRegNr);
             this.groupBox1.Location = new System.Drawing.Point(13, 13);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(775, 100);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
-            // textBox1
+            // btnAdd
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.textBox1.Location = new System.Drawing.Point(113, 19);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 23);
-            this.textBox1.TabIndex = 0;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnAdd.Location = new System.Drawing.Point(610, 59);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(106, 28);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
-            // textBox2
+            // label4
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.textBox2.Location = new System.Drawing.Point(113, 62);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 23);
-            this.textBox2.TabIndex = 1;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(305, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Year";
             // 
-            // textBox3
+            // label3
             // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.textBox3.Location = new System.Drawing.Point(373, 19);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 23);
-            this.textBox3.TabIndex = 2;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(305, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Model";
             // 
-            // textBox4
+            // label2
             // 
-            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.textBox4.Location = new System.Drawing.Point(373, 62);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 23);
-            this.textBox4.TabIndex = 3;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(57, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Make";
             // 
-            // checkBox1
+            // label1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.checkBox1.Location = new System.Drawing.Point(610, 21);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 21);
-            this.checkBox1.TabIndex = 4;
-            this.checkBox1.Text = "For Sale";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(57, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Reg Nr";
+            // 
+            // cbxForSale
+            // 
+            this.cbxForSale.AutoSize = true;
+            this.cbxForSale.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.cbxForSale.Location = new System.Drawing.Point(610, 21);
+            this.cbxForSale.Name = "cbxForSale";
+            this.cbxForSale.Size = new System.Drawing.Size(80, 21);
+            this.cbxForSale.TabIndex = 4;
+            this.cbxForSale.Text = "For Sale";
+            this.cbxForSale.UseVisualStyleBackColor = true;
+            // 
+            // tbxYear
+            // 
+            this.tbxYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tbxYear.Location = new System.Drawing.Point(373, 62);
+            this.tbxYear.Name = "tbxYear";
+            this.tbxYear.Size = new System.Drawing.Size(100, 23);
+            this.tbxYear.TabIndex = 3;
+            // 
+            // tbxModel
+            // 
+            this.tbxModel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tbxModel.Location = new System.Drawing.Point(373, 19);
+            this.tbxModel.Name = "tbxModel";
+            this.tbxModel.Size = new System.Drawing.Size(100, 23);
+            this.tbxModel.TabIndex = 2;
+            // 
+            // tbxMake
+            // 
+            this.tbxMake.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tbxMake.Location = new System.Drawing.Point(113, 62);
+            this.tbxMake.Name = "tbxMake";
+            this.tbxMake.Size = new System.Drawing.Size(100, 23);
+            this.tbxMake.TabIndex = 1;
+            // 
+            // tbxRegNr
+            // 
+            this.tbxRegNr.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tbxRegNr.Location = new System.Drawing.Point(113, 19);
+            this.tbxRegNr.Name = "tbxRegNr";
+            this.tbxRegNr.Size = new System.Drawing.Size(100, 23);
+            this.tbxRegNr.TabIndex = 0;
             // 
             // lsvCars
             // 
@@ -127,80 +178,6 @@
             this.lsvCars.UseCompatibleStateImageBehavior = false;
             this.lsvCars.View = System.Windows.Forms.View.Details;
             this.lsvCars.SelectedIndexChanged += new System.EventHandler(this.lsvCars_SelectedIndexChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label1.Location = new System.Drawing.Point(57, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 17);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Reg Nr";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label2.Location = new System.Drawing.Point(57, 62);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 17);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Make";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label3.Location = new System.Drawing.Point(305, 19);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Model";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label4.Location = new System.Drawing.Point(305, 62);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 17);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Year";
-            // 
-            // btnAdd
-            // 
-            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.btnAdd.Location = new System.Drawing.Point(610, 59);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(106, 28);
-            this.btnAdd.TabIndex = 9;
-            this.btnAdd.Text = "Add";
-            this.btnAdd.UseVisualStyleBackColor = true;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
-            // 
-            // btnClearAll
-            // 
-            this.btnClearAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.btnClearAll.Location = new System.Drawing.Point(73, 405);
-            this.btnClearAll.Name = "btnClearAll";
-            this.btnClearAll.Size = new System.Drawing.Size(144, 28);
-            this.btnClearAll.TabIndex = 2;
-            this.btnClearAll.Text = "Clear all";
-            this.btnClearAll.UseVisualStyleBackColor = true;
-            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
-            // 
-            // btnClearMarked
-            // 
-            this.btnClearMarked.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.btnClearMarked.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnClearMarked.Location = new System.Drawing.Point(584, 405);
-            this.btnClearMarked.Name = "btnClearMarked";
-            this.btnClearMarked.Size = new System.Drawing.Size(145, 28);
-            this.btnClearMarked.TabIndex = 3;
-            this.btnClearMarked.Text = "Clear marked";
-            this.btnClearMarked.UseVisualStyleBackColor = true;
-            this.btnClearMarked.Click += new System.EventHandler(this.btnClearMarked_Click);
             // 
             // regnr
             // 
@@ -222,12 +199,35 @@
             // 
             this.forsale.Text = "For Sale";
             // 
+            // btnClearAll
+            // 
+            this.btnClearAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnClearAll.Location = new System.Drawing.Point(73, 405);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(144, 28);
+            this.btnClearAll.TabIndex = 2;
+            this.btnClearAll.Text = "Clear all";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+            // 
+            // btnRemoveMarked
+            // 
+            this.btnRemoveMarked.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnRemoveMarked.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRemoveMarked.Location = new System.Drawing.Point(584, 405);
+            this.btnRemoveMarked.Name = "btnRemoveMarked";
+            this.btnRemoveMarked.Size = new System.Drawing.Size(145, 28);
+            this.btnRemoveMarked.TabIndex = 3;
+            this.btnRemoveMarked.Text = "Remove marked";
+            this.btnRemoveMarked.UseVisualStyleBackColor = true;
+            this.btnRemoveMarked.Click += new System.EventHandler(this.btnRemoveMarked_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnClearMarked);
+            this.Controls.Add(this.btnRemoveMarked);
             this.Controls.Add(this.btnClearAll);
             this.Controls.Add(this.lsvCars);
             this.Controls.Add(this.groupBox1);
@@ -249,11 +249,11 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox cbxForSale;
+        private System.Windows.Forms.TextBox tbxYear;
+        private System.Windows.Forms.TextBox tbxModel;
+        private System.Windows.Forms.TextBox tbxMake;
+        private System.Windows.Forms.TextBox tbxRegNr;
         private System.Windows.Forms.ListView lsvCars;
         private System.Windows.Forms.ColumnHeader regnr;
         private System.Windows.Forms.ColumnHeader make;
@@ -261,7 +261,7 @@
         private System.Windows.Forms.ColumnHeader year;
         private System.Windows.Forms.ColumnHeader forsale;
         private System.Windows.Forms.Button btnClearAll;
-        private System.Windows.Forms.Button btnClearMarked;
+        private System.Windows.Forms.Button btnRemoveMarked;
     }
 }
 
--- CarApp/Form1.cs
+++ CarApp/Form1.cs
@@ -15,26 +15,68 @@ namespace CarApp
         public Form1()
         {
             InitializeComponent();
+            tbxRegNr.Focus();
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrEmpty(tbxRegNr.Text) || string.IsNullOrEmpty(tbxMake.Text))
+            {
+                MessageBox.Show("You have to fill all the boxes", "Invalid input");
+            }
+            else
+            {
+                ListViewItem item = CreateListViewItem(tbxRegNr.Text, tbxMake.Text, tbxModel.Text, tbxYear.Text, cbxForSale.Checked);
+                lsvCars.Items.Add(item);
+                ClearTextboxes();
+                btnClearAll.Enabled = true;
+            }
+        }
 
+        private ListViewItem CreateListViewItem(string regNr, string make, string model, string year, bool forSale)
+        {
+            ListViewItem item = new ListViewItem(regNr);
+            item.SubItems.Add(make);
+            item.SubItems.Add(model);
+            item.SubItems.Add(year);
+            item.SubItems.Add(forSale ? "Yes" : "No");
+            return item;
         }
 
         private void btnClearAll_Click(object sender, EventArgs e)
         {
-
+            lsvCars.Items.Clear();
+            tbxRegNr.Focus();
         }
 
-        private void btnClearMarked_Click(object sender, EventArgs e)
+        private void btnRemoveMarked_Click(object sender, EventArgs e)
         {
-
+            if (lsvCars.SelectedItems.Count > 0)
+            {
+                var item = lsvCars.SelectedItems[0];
+                lsvCars.Items.Remove(item);
+                MessageBox.Show("The car with registration number '" + item.Text + "' is removed", "Removal of vehicle");
+            }
+            else
+            {
+                MessageBox.Show("No car was highlighted to be removed", "Removal of vehicle");
+            }
+            btnClearAll.Enabled = (lsvCars.Items.Count > 0);
         }
 
         private void lsvCars_SelectedIndexChanged(object sender, EventArgs e)
         {
+            btnRemoveMarked.Enabled = (lsvCars.SelectedItems.Count > 0);
+        }
 
+        private void ClearTextboxes()
+        {
+            tbxRegNr.Clear();
+            tbxMake.Clear();
+            tbxModel.Clear();
+            tbxYear.Clear();
+            cbxForSale.Checked = false;
+            tbxRegNr.Focus();
         }
     }
 }
